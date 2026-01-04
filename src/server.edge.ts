5e0ae1cc5b61647b8e61ba333408bf2ab0ad7a87
Cong-Cong <dacongsama@live.com>
2026-01-04 14:13:33 +0800

    feat: validateServerActions

--- src/server.edge.ts
+++ src/server.edge.ts
@@ -119,3 +119,17 @@ export function createServerEntry<T>(
   }
   return value as ServerEntry<T>;
 }
+
+// This function ensures that all the exported values are valid server actions,
+// during the runtime. By definition all actions are required to be async
+// functions, but here we can only check that they are functions.
+export function ensureServerActions(actions: any[]) {
+  for (let i = 0; i < actions.length; i++) {
+    const action = actions[i];
+    if (typeof action !== 'function') {
+      throw new Error(
+        `A "use server" file can only export async functions, found ${typeof action}.`,
+      );
+    }
+  }
+}
